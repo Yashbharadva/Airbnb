d20ab40ca14bf7deafc57c476ae87b38f9c992ea
studiomac-152 <studiomac-152@studiomac-152s-iMac.local>
2022-04-05 17:56:14 +0530

    Add footer section with the different components.

--- src/component/footer/footer.component.jsx
+++ src/component/footer/footer.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Link } from 'react-router-dom';
+
 
 import './footer.styles.scss';
 
@@ -9,16 +9,97 @@ const Footer = () => {
         <div className="footer">
             <div className="container1">
                 <h4>Support</h4>
+                <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Help Center
+                    </a> </div> 
+                    <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Safety Information
+                    </a> </div>
+                    <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Cancellation options
+                    </a> </div>
+                    <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Our COVID-19 Response
+                    </a> </div>
+                    <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Supporting people with disabilities
+                    </a> </div>
+                    <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Report a neighbourhood concern
+                    </a> </div>
+                
                 
             </div>
             <div className="container2">
                 <h4>Community</h4>
+                <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Airbnb.org: disaster relief housing
+                    </a> </div>
+                    <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Support Afghan refugees
+                    </a> </div>
+                    <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Combating discrimination
+                    </a> </div>
             </div>
             <div className="container3">
                 <h4>Hosting</h4>
+                <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Try hosting
+                    </a> </div>
+                    <div className="abc">
+                    <a href="/help/home?from=footer">
+                        AirCover:protection for Hosts
+                    </a> </div>
+                    <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Explore hosting resources
+                    </a> </div>
+                    <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Visit our community forum
+                    </a> </div>
+                    <div className="abc">
+                    <a href="/help/home?from=footer">
+                        How to host responsibly
+                    </a> </div>
             </div>
             <div className="container4">
                 <h4>About</h4>
+                <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Newsroom
+                    </a> </div>
+                    <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Learn about new features
+                    </a> </div>
+                    <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Letter from our founders
+                    </a> </div>
+                    <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Careers
+                    </a> </div>
+                    <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Investors
+                    </a> </div>
+                    <div className="abc">
+                    <a href="/help/home?from=footer">
+                        Airbnb Luxe
+                    </a> </div>
             </div>
         </div>
     )
