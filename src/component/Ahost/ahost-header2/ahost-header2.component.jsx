f3d6b28eb949df3a086342ddf276ac0469d71697
studiomac-152 <studiomac-152@studiomac-152s-iMac.local>
2022-04-29 14:41:49 +0530

    Apply animation with condition on after the complete the video page it should be change the color and change into the black to white after completing the two page which is main page and become a host page.

--- src/component/Ahost/ahost-header2/ahost-header2.component.jsx
+++ src/component/Ahost/ahost-header2/ahost-header2.component.jsx
@@ -7,9 +7,9 @@ import './ahost-header2.styles.scss';
 const AhostHeader2 = () => {
     const [header3, setHeader] = useState("header");
     const listenScrollEvent = event => {
-        if (window.scrollY > 900) {
+        if (window.scrollY > 1000) {
             return setHeader("header3");
-        } else if (window.scrollY < 500) {
+        } else if (window.scrollY < 900) {
             return setHeader("headerahost2");
         }
     };
@@ -30,10 +30,31 @@ const AhostHeader2 = () => {
                         <SiAirbnb size='2em' />
                     </a>
                     <div className="tryhosting">
-                    Try Button
+                    Try hosting
                 </div>
                 </div>
-                
+                <div className="main-pic-text">
+                <div className="pic-text">
+                    <div className="pic" >
+                        <img src ="https://a0.muscache.com/im/pictures/c131fb36-f46a-464f-ad2f-087ebf88078d.jpg"
+                        width='30px'
+                        alt="1st-pic" />
+                    </div>
+                    <div className="pic1" >
+                        <img src ="	https://a0.muscache.com/im/pictures/3ddc6e92-e2fd-4cdc-a460-2f1d7d5365ae.jpg"
+                        width='30px'
+                        alt="1st-pic" />
+                    </div>
+                    <div className="pic2" >
+                        <img src ="https://a0.muscache.com/im/pictures/ba6627db-1aa4-4f7f-9f18-5be3d3470037.jpg"
+                        width='30px'
+                        alt="1st-pic" />
+                    </div>
+                    <div className="text-pic">
+                    Ask a Superhost
+                    </div>
+                </div>
+                </div>
             </div>
         </div>
         </header>
