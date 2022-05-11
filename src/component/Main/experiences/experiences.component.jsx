38448e8007793080e633c385d3d483244553a71a
studiomac-152 <studiomac-152@studiomac-152s-iMac.local>
2022-05-11 17:58:17 +0530

    Complete the trip component with the responsive title and also complete the experience component.

--- src/component/Main/experiences/experiences.component.jsx
+++ src/component/Main/experiences/experiences.component.jsx
@@ -15,26 +15,26 @@ const Experiences = () => {
                     src="https://a0.muscache.com/im/pictures/b2f98185-f3bf-40db-ba8d-da0bceeccc65.jpg?im_w=720"
                     alt="" />
                     </div>
-                {/* <div className="container">
+                <div className="container">
                     <h1>Things to do <br />
                         on your trip</h1>
                     <div className="button" to="experiences">
                         <h3>Experiences</h3>
                     </div>
-                </div> */}
+                </div>
                 <div className="image2">
                 <Exe
                     src="https://a0.muscache.com/im/pictures/cae7ae9a-d069-4c6a-9267-795643472df1.jpg?im_w=720"
                     alt="" />
                     </div>
-                {/* <div className="containerr">
+                <div className="containerr">
                     <h1>Things to do<br />
                         from home 
                         </h1>
                     <Link className="buttonn" to="/online-experience">
                         <h3>Online Experiences</h3>
                     </Link>
-                </div> */}
+                </div>
 
 
             </div>
